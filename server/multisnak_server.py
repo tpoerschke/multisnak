0f1dd03e85d30e297639fcfcaa257b71f188c641
Tim Poerschke <info@coder-tim.de>
2020-03-06 22:06:06 +0100

    [FEATURE] Server akzeptiert bis zu 4 Spieler

--- server/multisnak_server.py
+++ server/multisnak_server.py
@@ -18,7 +18,26 @@ class Server(object):
         self.player_list = []
 
     def connect_clients(self):
-         # Bind und Listen
+
+        # Spieleranzahl abfragen 
+        print("Wie viele Spieler spielen mit? (1 - 4 / 0 -> Abbruch)")
+        correct_input = False
+        player_count = 0
+        while not correct_input:
+            try:
+                player_count = int(input(">> "))
+                if player_count < 0 or player_count > 4:
+                    raise ValueError
+                else:
+                    correct_input = True
+            except: 
+                print("Eingabe ungültig (1 - 4 / 0 -> Abbruch)")
+
+        # ggf. aussteigen
+        if player_count == 0:
+            sys.exit()
+
+        # Bind und Listen
         try:
             self.serverSocket.bind(("localhost", CONFIG["port"]))
             self.serverSocket.listen()
@@ -26,26 +45,35 @@ class Server(object):
             print("ERROR " + str(msg))
             sys.exit()
 
-        info("Warte auf eine Verbindungsanfrage")
-        self.serverSocket.settimeout(CONFIG["connectionTimeout"])
-        debug(f"Wartezeit: {CONFIG['connectionTimeout']} Sekunden")
+        player_con_threads = []
+        for i in range(player_count):
+            debug("Warte auf eine Verbindungsanfrage")
+            self.serverSocket.settimeout(CONFIG["connectionTimeout"])
+            debug(f"Wartezeit: {CONFIG['connectionTimeout']} Sekunden")
 
-        # TODO: Schön machen
-        try:
-            client_connected, client_address = self.serverSocket.accept()
-            debug("Akzeptiert eine Verbindungsanfrage von %s:%s" % (client_address[0], client_address[1]))
-            # Verbindung in Thread abwickeln, damit Anfragen schneller angenommen werden können
-            threading.Thread(target=self.__connect_player, args=(client_connected, client_address)).start()
-        except socket.error as err:
+            # TODO: Schön machen
+            try:
+                client_connected, client_address = self.serverSocket.accept()
+                debug("Akzeptiert eine Verbindungsanfrage von %s:%s" % (client_address[0], client_address[1]))
+                # Verbindung in Thread abwickeln, damit Anfragen schneller angenommen werden können
+                t = threading.Thread(target=self.__connect_player, args=(client_connected, client_address))
+                t.start()
+                player_con_threads.append(t)
+            except socket.error as err:
+                print("WARNING Fehler bei der Verbindung mit Spieler " + str(len(self.player_list) + 1) + "!")
+
+        if len(self.player_list) == 0:
             info("Kein Spieler verbunden!")
             self.serverSocket.close()
             sys.exit()
-
-        #self.client_connected = client_connected
-        time.sleep(5) # Das Verbinden anderer Spieler simulieren
+        else:
+            for t in player_con_threads: t.join()
+            info(str(len(self.player_list)) + " Spieler verbunden!")
+            time.sleep(5) 
 
     def __connect_player(self, client_connected, client_address):
-        new_player = Player(1, "Spieler 1", client_connected, client_address)
+        player_id = len(self.player_list) + 1
+        new_player = Player(player_id, f"Spieler {player_id}", client_connected, client_address)
         info(f"Verbunden: {new_player.name} ({new_player.address[0]})")
         self.player_list.append(new_player)
 
@@ -58,7 +86,6 @@ class Server(object):
     def do_game_loop(self):
         try:
             # Start-Mechanismen des Client anstoßen
-            #self.client_connected.send()
             for player in self.player_list:
                 player.socket.send(str.encode("{\"state\": \"prepare\"}"))
                 debug(f"\"prepare\"-State an {player.name} gesendet.")
